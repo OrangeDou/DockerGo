101d3a19044a55f4610cac322f0b8f1b8d3ae486
文强 <wenqiangsspu@163.com>
2024-07-26 21:22:52 +0800

    update: init command

---
+++ command.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/urfave/cli"
+
+var runCommand = cli.Command{
+	Name:  "run",
+	Usage: `Create a container with namespace and cgroups limit ie: mydocker run -ti [image] [command]`,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "ti",
+			Usage: "enable tty",
+		},
+	},
+	// run function
+	Action: ,
+}
