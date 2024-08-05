48ec029ddf158e75d3a5821f82cd1bb77f86691c
文强 <wenqiangsspu@163.com>
2024-08-05 10:09:01 +0800

    add: cgroup Manager

--- cgroups/subsystems/subsystem.go
+++ cgroups/subsystems/subsystem.go
@@ -10,7 +10,7 @@ type ResourceConfig struct {
 type Subsystem interface {
 	Name() string                               // 返回subsystem的名字
 	Set(path string, res *ResourceConfig) error // 设置某个cgroup在subsystem的资源限制
-	Apply(path string, pid int) error           // 将进程添加到某个cgroup中
+	Apply(path string, pid int) error           // 将进程添加到某个cgroup中（将进程ID写入cgroup的cgroup.procs文件）
 	Remove(path string) error                   //移除某个cgroup
 }
 
