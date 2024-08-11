99b986ab9a8f3c9d2800a448231119a53adbcc53
OrangeDou <wenqiangsspu@163.com>
2024-08-11 17:30:10 +0800

    add: docker ps

--- run.go
+++ run.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"gocker/cgroups"
 	"gocker/cgroups/subsystems"
 	"gocker/container"
+	"math/rand"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig, containerName, volume, imageName string) {
+	containerID := randStringBytes(10)
+	if containerID == "" {
+		containerName = containerID
+	}
+
 	parent, writePipe := container.NewParentProcess(tty)
 	if parent == nil {
 		log.Errorf("New parent process error")
@@ -19,7 +29,12 @@ func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
-
+	// 记录容器信息
+	containerName, err := recordContainerInfo(parent.Process.Pid, comArray, containerName, containerID, volume)
+	if err != nil {
+		log.Errorf("Record container info error", err)
+		return
+	}
 	// docker-go as a cgroup name
 	// 创建cgroupmanager，调用set和apply设置资源限制并使限制在容器上生效
 	cgroupManager := cgroups.NewCgroupManager("docker-go")
@@ -39,4 +54,61 @@ func sendInitCommand(comArray []string, writePipe *os.File) {
 	log.Infof("command all is %s", command)
 	writePipe.WriteString(command)
 	writePipe.Close()
+}
+func randStringBytes(n int) string {
+	letterBytes := "1234567890"
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+// 记录容器信息
+func recordContainerInfo(containerPID int, commandArray []string, containerName, id, volume string) (string, error) {
+	createTime := time.Now().Format("2006-01-02 15:04:05")
+	command := strings.Join(commandArray, "")
+	containerInfo := &container.ContainerInfo{
+		Id:          id,
+		Pid:         strconv.Itoa(containerPID),
+		Command:     command,
+		CreatedTime: createTime,
+		Status:      container.RUNNING,
+		Name:        containerName,
+		Volume:      volume,
+	}
+
+	jsonBytes, err := json.Marshal(containerInfo)
+	if err != nil {
+		log.Errorf("Record container info error %v", err)
+		return "", err
+	}
+	jsonStr := string(jsonBytes)
+
+	dirUrl := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	if err := os.MkdirAll(dirUrl, 0622); err != nil {
+		log.Errorf("Mkdir error %s error %v", dirUrl, err)
+		return "", err
+	}
+	fileName := dirUrl + "/" + container.ConfigName
+	file, err := os.Create(fileName)
+	defer file.Close()
+	if err != nil {
+		log.Errorf("Create file %s error %v", fileName, err)
+		return "", err
+	}
+	if _, err := file.WriteString(jsonStr); err != nil {
+		log.Errorf("File write string error %v", err)
+		return "", err
+	}
+
+	return containerName, nil
+}
+
+func deleteContainerInfo(containerId string) {
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerId)
+	if err := os.RemoveAll(dirURL); err != nil {
+		log.Errorf("Remove dir %s error %v", dirURL, err)
+	}
 }
