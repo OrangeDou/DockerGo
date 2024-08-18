524b20bd7dbaa8f93f2cf531cd7cfc5c10f6c071
文强 <wenqiangsspu@163.com>
2024-08-18 16:01:24 +0800

    update: volumn

---
+++ container/volume.go
@@ -0,0 +1,63 @@
+package container
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Delete the AUFS filesystem while container exit
+func DeleteWorkSpace(volume, containerName string) {
+	if volume != "" {
+		volumeURLs := strings.Split(volume, ":")
+		length := len(volumeURLs)
+		if length == 2 && volumeURLs[0] != "" && volumeURLs[1] != "" {
+			DeleteVolume(volumeURLs, containerName)
+		}
+	}
+	DeleteMountPoint(containerName)
+	DeleteWriteLayer(containerName)
+}
+
+func DeleteMountPoint(containerName string) error {
+	mntURL := fmt.Sprintf(MntUrl, containerName)
+	_, err := exec.Command("umount", mntURL).CombinedOutput()
+	if err != nil {
+		log.Errorf("Unmount %s error %v", mntURL, err)
+		return err
+	}
+	if err := os.RemoveAll(mntURL); err != nil {
+		log.Errorf("Remove mountpoint dir %s error %v", mntURL, err)
+		return err
+	}
+	return nil
+}
+func DeleteVolume(volumeURLs []string, containerName string) error {
+	mntURL := fmt.Sprintf(MntUrl, containerName)
+	containerUrl := mntURL + "/" + volumeURLs[1]
+	if _, err := exec.Command("umount", containerUrl).CombinedOutput(); err != nil {
+		log.Errorf("Umount volume %s failed. %v", containerUrl, err)
+		return err
+	}
+	return nil
+}
+
+func DeleteWriteLayer(containerName string) {
+	writeURL := fmt.Sprintf(WriteLayerUrl, containerName)
+	if err := os.RemoveAll(writeURL); err != nil {
+		log.Infof("Remove writeLayer dir %s error %v", writeURL, err)
+	}
+}
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
